83359d8d7630be58da0f27ee29d2822a2f8cb25c
razaalikhan123 <razakhanboss519@gmail.com>
2025-03-09 00:15:17 +0500

    Updates

---
+++ precache-manifest.e1ca77c53d1f0ba589a73a9ec457876d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afae657ead03e0b7530bd0270d013f3e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ca49613a627efdb93e68",
+    "url": "/Portfolio/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "97d68cc723a66aff6493",
+    "url": "/Portfolio/static/css/main.f0f8db55.chunk.css"
+  },
+  {
+    "revision": "ca49613a627efdb93e68",
+    "url": "/Portfolio/static/js/2.060b4713.chunk.js"
+  },
+  {
+    "revision": "5ac06fec4121bd76f82f",
+    "url": "/Portfolio/static/js/3.35f637f7.chunk.js"
+  },
+  {
+    "revision": "97d68cc723a66aff6493",
+    "url": "/Portfolio/static/js/main.8c981c72.chunk.js"
+  },
+  {
+    "revision": "4b215633915d255974c9",
+    "url": "/Portfolio/static/js/runtime~main.63f5289f.js"
+  },
+  {
+    "revision": "6f40ed6a704c62f53f8c0c19869b4c3d",
+    "url": "/Portfolio/static/media/Raza-cv.6f40ed6a.pdf"
+  },
+  {
+    "revision": "f72c9484379cdee43a67a8aa8c59c485",
+    "url": "/Portfolio/static/media/cargo.f72c9484.jpg"
+  },
+  {
+    "revision": "68231516c01be0a55486a9af7b4b2b3b",
+    "url": "/Portfolio/static/media/faysal cert.68231516.png"
+  },
+  {
+    "revision": "87db5aec0f42bb575723b3a431c523a1",
+    "url": "/Portfolio/static/media/flick.87db5aec.jpg"
+  },
+  {
+    "revision": "7a26883521c66ad514bc0fd94cdb32f1",
+    "url": "/Portfolio/static/media/focus first.7a268835.jpg"
+  },
+  {
+    "revision": "fed9a7c7dc535bdc3cb5355496734ed9",
+    "url": "/Portfolio/static/media/paysys cert.fed9a7c7.png"
+  },
+  {
+    "revision": "d314cc2b0e2abeafaa60ea2881c5f4ec",
+    "url": "/Portfolio/static/media/r2.d314cc2b.jpg"
+  },
+  {
+    "revision": "d7e3807e91a7f8fd2d87ffec44535aef",
+    "url": "/Portfolio/static/media/spacex cert.d7e3807e.png"
+  }
+]);
